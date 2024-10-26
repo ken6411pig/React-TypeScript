41f57c34be4c4efbf2287262e87ea8bb667c0b5e
ken6411pig <ken6411pig@gmail.com>
2024-10-26 17:00:15 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/React-TypeScript/',  // 添加這行
   server: {
     host: '0.0.0.0',
   }
